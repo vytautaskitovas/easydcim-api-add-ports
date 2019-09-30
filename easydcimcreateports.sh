2b72c97c2f189a49d99b811159e24864baebe004
Vytautas Kitovas <38241840+vytautaskitovas@users.noreply.github.com>
2019-09-30 09:32:55 +0300

    Update easydcimcreateports.sh

--- easydcimcreateports.sh
+++ easydcimcreateports.sh
@@ -2,8 +2,8 @@
 echo "Enter device ID'e each in a new line and press Enter, then press CTRL + D to start adding PORTS"
 deviceslist=`cat`
 LAN="curl -s -g --request POST --url 'https://dcim.heficed.com/api/v2/device/"
-LAN1="/port?data[port_number]=0&data[port_label]=LAN1&data[description]=NIC-A&data[speed]=1000000000&data[pullable]=1' --header 'apikey: ' | grep "success""
-LAN2="/port?data[port_number]=0&data[port_label]=LAN2&data[description]=NIC-B&data[speed]=1000000000&data[pullable]=1' --header 'apikey: ' | grep "success""
+LAN1="/port?data[port_number]=0&data[port_label]=LAN1&data[description]=NIC-A&data[speed]=1000000000&data[pullable]=1' --header 'apikey: ' "
+LAN2="/port?data[port_number]=0&data[port_label]=LAN2&data[description]=NIC-B&data[speed]=1000000000&data[pullable]=1' --header 'apikey: ' "
 for deviceid in $deviceslist
 do
 
